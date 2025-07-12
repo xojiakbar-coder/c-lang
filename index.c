5ca0d958927549a6fd5e3266cd61ab67f854f273
Xojiakbar <xojiakbarisroilov.777@gmail.com>
2025-07-12 09:46:26 +0500

    odd/even number detection program

--- index.c
+++ index.c
@@ -4,8 +4,7 @@ int main(void) {
     int value;
     printf("Son kiriting: ");
     scanf("%d", &value);
-    if(value > 0) printf("Musbat son: %d\n", value);
-    else if(value < 0) printf("Manfiy son: %d\n", value);
-    else if(value == 0) printf("Neytral son: %d\n", value);
+    if(value % 2 == 0) printf("Juft son: %d\n", value);
+    else printf("Toq son: %d\n", value);
     return 0;
 }
